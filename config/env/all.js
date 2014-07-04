8ca6b7c0a2c4609190737f96ac551f5c04415265
Murat Kucukosman <murat@copypanthers.com>
2014-07-04 13:07:32 +0300

    changed title

--- config/env/all.js
+++ config/env/all.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	app: {
-		title: 'Atlasam Gitsem',
+		title: 'Suxse | keep your wallet',
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
 		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
 	},
